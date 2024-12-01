b7133b173cf333e5a989ed408c269bc00d8f53e7
Slooma951 <b00149614@mytudublin.ie>
2024-12-01 18:42:09 +0000

    changes

--- my-app/src/lib/mongodb.js
+++ my-app/src/lib/mongodb.js
@@ -4,18 +4,28 @@ let client;
 let dbConnection;
 
 const connectToDatabase = async () => {
-    if (!dbConnection) {
-
+    try {
+        if (!dbConnection) {
             if (!client) {
-                client = new MongoClient(process.env.MONGODB_URI);
+                // Initialize the MongoClient only if it does not already exist
+                client = new MongoClient(process.env.MONGODB_URI, {
+                    useNewUrlParser: true,
+                    useUnifiedTopology: true,
+                });
             }
+
+            // Attempt connection
             await client.connect();
+
+            // Connect to the database
             dbConnection = client.db("RWADB");
             console.log("Database connected successfully");
-
-
+        }
+        return dbConnection;
+    } catch (error) {
+        console.error("Failed to connect to the database:", error);
+        throw new Error("Database connection error");
     }
-    return dbConnection;
 };
 
 export default connectToDatabase;
